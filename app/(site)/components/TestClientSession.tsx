c40edad981c1045988679b86915fa24a7cf92ee0
Piotr Kozlowski <pegaz@ante.pl>
2023-06-28 15:27:10 +0200

    fix: session error problem

--- app/(site)/components/TestClientSession.tsx
+++ app/(site)/components/TestClientSession.tsx
@@ -2,11 +2,11 @@
 import { useSession } from 'next-auth/react';
 
 export default function TestClientSession() {
-  const { data: session } = useSession();
+  const session = useSession();
   return (
     <div>
       <h1>TestClientSession</h1>
-      <pre>{JSON.stringify(session)}</pre>
+      <pre>{JSON.stringify(session.data)}</pre>
       <pre>{session?.status || 'no info'}</pre>
     </div>
   );
