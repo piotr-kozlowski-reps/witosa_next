19e200f28cb07819e9c987dc8c450be10b02d631
Piotr Kozlowski <pegaz@ante.pl>
2023-08-31 21:45:21 +0200

    feat: playing with error tsx

--- app/(site)/bistro/page.tsx
+++ app/(site)/bistro/page.tsx
@@ -1,18 +1,19 @@
 import { Metadata } from 'next';
-import { Fragment } from 'react';
-import FooterMain from '../components/footer/FooterMain';
-import BistroContent from './components/BistroContent';
+// import { Fragment } from 'react';
+// import FooterMain from '../components/footer/FooterMain';
+// import BistroContent from './components/BistroContent';
 
 export const metadata: Metadata = {
   title: 'Art Cafe | Art CK',
 };
 
 export default async function BistroPage() {
+  throw new Error('fgbdfgb');
   ////tsx
-  return (
-    <Fragment>
-      <BistroContent />
-      <FooterMain />
-    </Fragment>
-  );
+  // return (
+  //   <Fragment>
+  //     <BistroContent />
+  //     <FooterMain />
+  //   </Fragment>
+  // );
 }
--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,29 @@
 'use client';
 
-const Error = ({ error, reset }: { error: Error; reset: () => {} }) => {
-  window.location.replace(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+import Link from 'next/link';
+import { Fragment } from 'react';
 
-  return <div>loading...</div>;
+const Error = ({ error, reset }: { error: Error; reset: () => {} }) => {
+  // window.location.replace(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+
+  // return <div>loading...</div>;
+
+  return (
+    <Fragment>
+      <div>
+        <p>W aplikacji pojawił sie problem</p>
+      </div>
+      <div>
+        <h1>{error.message || 'Coś poszło nie tak'}</h1>
+      </div>
+      <div>
+        <button onClick={reset}>Spróbuj ponownie</button>
+      </div>
+      <div>
+        <Link href="/">powrót do początku</Link>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Error;
