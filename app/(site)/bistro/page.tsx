18791df235738a109f26925946966231390fd6a4
Piotr Kozlowski <pegaz@ante.pl>
2023-08-31 21:49:03 +0200

    feat: playing with errors

--- app/(site)/bistro/page.tsx
+++ app/(site)/bistro/page.tsx
@@ -1,19 +1,19 @@
 import { Metadata } from 'next';
-// import { Fragment } from 'react';
-// import FooterMain from '../components/footer/FooterMain';
-// import BistroContent from './components/BistroContent';
+import { Fragment } from 'react';
+import FooterMain from '../components/footer/FooterMain';
+import BistroContent from './components/BistroContent';
 
 export const metadata: Metadata = {
   title: 'Art Cafe | Art CK',
 };
 
 export default async function BistroPage() {
-  throw new Error('fgbdfgb');
-  ////tsx
-  // return (
-  //   <Fragment>
-  //     <BistroContent />
-  //     <FooterMain />
-  //   </Fragment>
-  // );
+  // throw new Error('fgbdfgb');
+  //tsx
+  return (
+    <Fragment>
+      <BistroContent />
+      <FooterMain />
+    </Fragment>
+  );
 }
