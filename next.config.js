1ce856bb2fd708793bcf2e35fb74ef8a06a5b33d
Piotr Kozlowski <pegaz@ante.pl>
2023-10-31 14:22:12 +0100

    fix: another try with next image

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'https://witosa.onrender.com',
+        hostname: 'witosa.onrender.com',
       },
     ],
   },
