57b38aedb645a5aa109d07eb170441d9e5778037
Piotr Kozlowski <pegaz@ante.pl>
2023-10-31 15:20:08 +0100

    fix: another try adding unoptimised to nextimage

--- app/(site)/components/forms/DropImageFormik.tsx
+++ app/(site)/components/forms/DropImageFormik.tsx
@@ -68,6 +68,7 @@ export function DropImageFormik<T>(props: Props<T>) {
         >
           {file ? (
             <Image
+              unoptimized
               src={src}
               width={64}
               height={64}
