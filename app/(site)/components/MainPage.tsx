44382716c000ef3f5662a53de4c52278f7e70bd7
Piotr Kozlowski <pegaz@ante.pl>
2023-11-04 10:53:47 +0100

    feat: technical break modal

--- app/(site)/components/MainPage.tsx
+++ app/(site)/components/MainPage.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useModalState } from '@/context/modalState';
 import { TEventInNewsSection, TSlide } from '@/types';
 import { motion } from 'framer-motion';
 import FooterMain from './footer/FooterMain';
+import ModalTechnicalBreak from './modal/ModalTechnicalBreak';
 import NewsMain from './news/NewsMain';
 import MainSlider from './slider_main/MainSlider';
 
@@ -14,6 +16,9 @@ type Props = {
 export default function MainPage(props: Props) {
   ////vars
   const { sliderData, newsData } = props;
+  const { setShowModal } = useModalState();
+
+  setShowModal(true, <ModalTechnicalBreak />);
 
   ////tsx
   return (
--- app/(site)/components/modal/ModalTechnicalBreak.tsx
+++ app/(site)/components/modal/ModalTechnicalBreak.tsx
@@ -1,35 +1,32 @@
 'use client';
 
 import { useModalState } from '@/context/modalState';
-import CustomButton from '../CustomButton';
 import ModalTitle from './ModalTitle';
 
 // type Props = {
 //   cyclicalActivities: { id: string; name: string }[];
 // };
 
-export default function ModalNotEnoughWidthForAdmin() {
+export default function ModalTechnicalBreak() {
   ////vars
   const { setHideModal } = useModalState();
 
   ////tsx
   return (
     <div className="max-w-full prose">
-      <ModalTitle text="Mamy problem :-(" />
+      <ModalTitle text="Przerwa techniczna" />
 
       <div>
+        <p>Trwają prace konserwacyjne, strona niedługo znów będzie dostępna.</p>
+
         <p>
-          By w pełni wykorzystać część administracyjną, szerokość ekranu powinna
-          być większa. <br /> Zalecana szerokość: <b>1140 pikseli w poziomie</b>
-          .
-        </p>
-        <p>
-          W innym przypadku część elementów może być niewidoczna w całości bądź
-          stać się nieczytelna.
+          Pozdrawiamy,
+          <br />
+          Zespół Art CK
         </p>
       </div>
 
-      <div className="flex flex-col tablet:flex-row justify-start items-start tablet:items-center gap-4 tablet:gap-8 mt-[25px]">
+      {/* <div className="flex flex-col tablet:flex-row justify-start items-start tablet:items-center gap-4 tablet:gap-8 mt-[25px]">
         <div>
           <CustomButton
             text="jestem świadom - przejdź dalej"
@@ -39,7 +36,7 @@ export default function ModalNotEnoughWidthForAdmin() {
             outlined={true}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
